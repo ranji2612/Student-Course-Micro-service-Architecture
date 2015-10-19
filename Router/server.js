59e471e736c21d7da4998f10bf22134a87d68ede
ranji2612 <ranji2612@gmail.com>
2015-10-18 23:51:22 -0400

    Multi instance and api gateway improvement

--- Router/server.js
+++ Router/server.js
@@ -8,15 +8,47 @@ var proxy = httpProxy.createProxyServer({});
 //// Listen for the `proxyRes` event on `proxy`.
 ////
 proxy.on('proxyRes', function (proxyRes, req, res) {
-  console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+  //console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
   if (req.method == 'GET' && req.url=='/api/getStudents') {
 	console.log('Yes this is good');
   }
 });
 
 server = http.createServer(function(req,res) {
-	console.log(req);
-	var target = {target : 'http://localhost:8080'};
-	proxy.proxyRequest(req, res, target);
+    
+    //By Default student Instance 1
+	var target = {target : 'http://localhost:9001'};
+    
+    var reqUrl = req.url.slice(1).split('/');
+    
+    //Based on the Last name, select the corresponding instance
+    //      A-I  -  Student Instance 1
+    //      J-Q  -  Student Instance 2
+    //      R-Z  -  Student Instance 3
+    if (reqUrl.length > 2 && reqUrl[0]==='api') {
+        console.log('Its an api call'); 
+        
+        //Convert to Lower case to make it simpler
+        var lastNameLetter = reqUrl[2][1].toLowerCase();
+        console.log('First Letter of Last Name ',lastNameLetter);
+        //      A-I  -  Student Instance 1
+        if ( lastNameLetter >= 'a' && lastNameLetter <= 'i' ) {
+            console.log('Navigating to Server 1');
+            target = {target : 'http://localhost:9001'};
+        }
+        if ( lastNameLetter >= 'j' && lastNameLetter <= 'q' ) {
+            console.log('Navigating to Server 2');
+            target = {target : 'http://localhost:9002'};
+        }
+        if ( lastNameLetter >= 'r' && lastNameLetter <= 'z' ) {
+            console.log('Navigating to Server 3');
+            target = {target : 'http://localhost:9003'};
+        }
+    }
+    
+    proxy.proxyRequest(req, res, target);
 });
+
+//Starting the server
+console.log('Starting the Router to server studentInstances');
 server.listen(8000);
