5b6ee202950976265ed625dabecc62198f95015f
ranji2612 <ranji2612@gmail.com>
2015-11-19 17:17:01 -0500

    Remvoving unwanted data from router

--- Router/server.js
+++ Router/server.js
@@ -8,10 +8,7 @@ var proxy = httpProxy.createProxyServer({});
 //// Listen for the `proxyRes` event on `proxy`.
 ////
 proxy.on('proxyRes', function (proxyRes, req, res) {
-  //console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
-  if (req.method == 'GET' && req.url=='/api/getStudents') {
-	console.log('Yes this is good');
-  }
+  
 });
 
 server = http.createServer(function(req,res) {
